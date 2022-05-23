4ab40027a1bed4f8fdadfe615e8597a450471d4a
Gabriel da Silva <gscruz.1997@gmail.com>
2022-05-22 23:58:34 -0300

    reforço

--- modulo1/reforco-js/index.js
+++ modulo1/reforco-js/index.js
@@ -1,25 +1,28 @@
 // Exercício 3
 
-const nomeDoUsuario = prompt('Insira seu nome')
-const anoDeNascimento = prompt ('Insira o ano que nasceu')
-const senhaDoUsuario = prompt ('Insira a senha')
-const nacionalidade = prompt ('Nacionalidade')
+const usuario = [
+    {nome:"Gabriel", idade: 25, senha: 123456, ano: 1997}
+]
 
-cadastro = [nomeDoUsuario, anoDeNascimento, senhaDoUsuario, nacionalidade]
+const login = Number(prompt("digite sua senha "))
 
-switch (cadastro){
-    case nomeDoUsuario:
-        console.log('Seja Bem Vindo')
-        break
-    case senhaDoUsuario:
-        console.log('Seja Bem Vindo')
-        break
-    case anoDeNascimento:
-        console.log('Seja Bem Vindo')
-        break
-    case nacionalidade:
-        console.log('Seja Bem Vindo')
-        break
-    default:
-        console.log('Você não pode se cadastrar')
+if (login === usuario.senha){
+    console.log('Usuario logado')
+}else {
+    console.log('Senha inválida')
 }
+
+ const primeiraDose = prompt('Informe seu nome')
+ const vacina = prompt ('Informe sua vacina')
+
+
+ if ('coronavac' === vacina){
+    console.log(`Olá ${primeiraDose}! A próxima dose da coronavac é daqui a 28 dias. Compareça no posto na data 29/08/2021.`)
+ } else if('astrazenica' === vacina){
+    console.log(`Olá ${primeiraDose}! A próxima dose da Astrazenica é daqui a 90 dias. Compareça no posto na data 30/10/2021.`)
+ } else if ('pfizer' === vacina){
+    console.log(`Olá ${primeiraDose}! A próxima dose da Pfizer é daqui a 90 dias. Compareça no posto na data 30/10/2021.`)
+ } else {
+     console.log(`Informe nome e vaicina novamente`)
+ }
+
