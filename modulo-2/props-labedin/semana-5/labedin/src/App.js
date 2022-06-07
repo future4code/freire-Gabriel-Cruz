6b456c2206b6d226c7568bd87ce1a457761c3f28
Gabriel da Silva <gscruz.1997@gmail.com>
2022-06-07 16:59:32 -0300

    fazendo-meu-curriculo

---
+++ modulo-2/props-labedin/semana-5/labedin/src/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import CardPequeno from './components/CardPequeno';
+import gabriel from './img/gabriel.JPG';
+import seta from './img/seta.jpg'
+import logonasa from './img/nasa.png'
+import email from './img/email.png'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessais</h2>
+        <CardGrande 
+          imagem= {gabriel}
+          nome="Gabriel da Silva Cruz" 
+          descricao="Oi, eu sou o Gabriel. Sou um dos alunos da Labenu. Eu estou desenvolvento conhecimento sobre JS, CSS e agora react."
+        />
+        
+        <ImagemButton 
+          imagem= {seta} 
+          texto="Ver mais"
+        />
+      </div>
+      <div className='dados'>
+        <CardPequeno
+        imagem= {email}
+        email= "Email:"
+        endereco= "gscruz.1997@gmail.com"
+        />
+      </div>
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Labenu" 
+          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+        />
+        
+        <CardGrande 
+          imagem= {logonasa} 
+          nome="NASA" 
+          descricao="Apontando defeitos." 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
